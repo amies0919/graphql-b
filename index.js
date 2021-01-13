9810f715dd24e24ea503e09c38606eba99a92954
shennana <shennana@jd.com>
2021-01-13 18:18:55 +0800

    初步添加数据库

--- index.js
+++ index.js
@@ -4,13 +4,25 @@ const expressPlayground = require('graphql-playground-middleware-express').defau
 const { readFileSync } = require('fs')
 const typeDefs = readFileSync('./typeDefs.graphql','utf8')
 const resolvers = require('./resolvers')
+const { MongoClient }  = require('mongodb')
+require('dotenv').config()
+//process.env.xxx
 
-const app = express()
-const server = new ApolloServer({
-    typeDefs,
-    resolvers
-})
-server.applyMiddleware({ app })
-app.get('/',(req, res)=>res.send('Welcome to the photoShare API'))
-app.get('/playground', expressPlayground({ endpoint:'/graphql' }))
-app.listen({port: 4000},() => console.log(`GraphQL Service running @ http://localhost:4000${server.graphqlPath}`))
+async function start() {
+    const app = express()
+    const DB_HOST = process.env.DB_HOST
+    const client = await MongoClient.connect(DB_HOST, {useNewUrlParser:true})
+    const db = client.db()
+    const context = { db }
+
+    const server = new ApolloServer({
+        typeDefs,
+        resolvers,
+        context
+    })
+    server.applyMiddleware({ app })
+    app.get('/', (req, res) => res.send('Welcome to the photoShare API'))
+    app.get('/playground', expressPlayground({ endpoint: '/graphql' }))
+    app.listen({ port: 4000 }, () => console.log(`GraphQL Service running @ http://localhost:4000${server.graphqlPath}`))
+}
+start()
--- resolvers/Query.js
+++ resolvers/Query.js
@@ -1,5 +1,6 @@
-var { photos } = require('./mock')
 module.exports = {
-    totalPhotos: () => photos.length,
-    allPhotos: () => photos
+    totalPhotos: (parent, args, { db }) => db.collection('photos').estimatedDocumentCount(),
+    allPhotos: (parent, args, { db }) => db.collection('photos').find().toArray(),
+    totalUsers: (parent, args, { db })=> db.collection('users').estimatedDocumentCount(),
+    allUsers: (parent, args, { db })=>db.collection('users').find().toArray()
 }
